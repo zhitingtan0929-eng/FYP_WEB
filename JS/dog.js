9cef75da71707954e2cbeebb117369ba37ed15f6
tanzhiting <zhiting233071@dasein.edu.my>
2026-08-26 15:04:19 +0800

    question styling

--- JS/dog.js
+++ JS/dog.js
@@ -11,40 +11,58 @@ const dogQuestion = [
             {
                 name: "Slim Build",
                 imageID: "body1",
+                buttonImage: "../img/dog/body/body1.png",
                 risk: "Low",
                 //score: 0,
-                problems: []
+                problems: [],
+                problemImage: "",
+                breed: "Greyhound / Whippet",
+                link: ""
             },
             {
                 name: "Square Build",
                 imageID: "body2",
+                buttonImage: "../img/dog/body/body2.png",
                 risk: "Medium",
                 //score: 1,
                 problems: [
-                    "Joint stress"
-                ]
+                    "Hip/Elbow disease",
+                    "Osteoarthritis risk"
+                ],
+                problemImage: "../img/dog/problems/body/body2-problem.png",
+                breed: "Rottweiler",
+                link: "https://heropethealth.com.au/blog/rottweiler-health-problems-prevention-and-care"
             },
             {
                 name: "Stocky Build",
                 imageID: "body3",
+                buttonImage: "../img/dog/body/body3.png",
                 risk: "Medium",
                 //score: 1,
                 problems: [
-                    "Weight gain",
-                    "Hip problems",
+                    "Increased body weight",
+                    "Hip dysplasia",
                     "Joint pain"
-                ]
+                ],
+                problemImage: "../img/dog/problems/body/body3-problem.png",
+                breed: "English Bulldog",
+                link: "https://pubmed.ncbi.nlm.nih.gov/11767921/"
             },
             {
                 name: "Compact Build",
                 imageID: "body4",
+                buttonImage: "../img/dog/body/body4.png",
                 risk: "High",
                 //score: 2,
                 problems: [
-                    "reduced airway space",
+                    "reduced airway space/ BOAS",
                     "overheating risk",
-                    "Back problems"
-                ]
+                    "Exercise intolerance",
+                    "Back / spinal problems"
+                ],
+                problemImage: "../img/dog/problems/body/body4-problem.png",
+                breed: "Pug / Pekingese",
+                link: "https://healthtopics.vetmed.ucdavis.edu/health-topics/brachycephalic-short-nosed-dogs"
             }
         ]
     },
@@ -58,39 +76,55 @@ const dogQuestion = [
             {
                 name: "Almond-shaped Eyes",
                 imageID: "eye1",
+                buttonImage: "../img/dog/eye/eye1.png",
                 risk: "Low",
                 //score: 0,
-                problems: []
+                problems: [],
+                problemImage: "",
+                breed: "",
+                link: ""
             },
             {
                 name: "Round Eyes",
                 imageID: "eye2",
+                buttonImage: "../img/dog/eye/eye2.png",
                 risk: "Low",
                 //score: 0,
-                problems: []
+                problems: [],
+                problemImage: "",
+                breed: "",
+                link: ""
             },
             {
-                name: "Triangular Eyes",
+                name: "Sunken eyes",
                 imageID: "eye3",
+                buttonImage: "../img/dog/eye/eye3.png",
                 risk: "Medium",
                 //score: 1,
                 problems: [
-                    "Dry eyes risk",
-                    "Eye irritation",
-                    "Mild entropion risk"
-                ]
+                    "Entropion",
+                    "Corneal irritation",
+                    "Corneal ulcer"
+                ],
+                problemImage: "../img/dog/problems/eye/eye3-problem.png",
+                breed: "Shar-Pei / Chow Chow ",
+                link: "https://www.acvo.org/tips-treatments-tricks/entropion"
             },
             {
                 name: "Protruding Eyes",
                 imageID: "eye4",
+                buttonImage: "../img/dog/eye/eye4.png",
                 risk: "High",
                 //score: 1,
                 problems: [
-                    "Corneal injury",
-                    "Dry eyes",
+                    "Dry eye / KCS",
+                    "Proptosis",
                     "Eye trauma risk",
-                    //"incomplete blinking"
-                ]
+                    "Vision impairment"
+                ],
+                problemImage: "../img/dog/problems/eye/eye4-problem.png",
+                breed: "Chihuahua / Pekingese",
+                link: "https://www.rvc.ac.uk/research/focus/brachycephaly/health-issues/ophthalmology"
             }
         ]
     },
@@ -104,39 +138,52 @@ const dogQuestion = [
             {
                 name: "Prick Ears",
                 imageID: "ear1",
+                buttonImage: "../img/dog/ear/ear1.png",
                 risk: "Low",
                 //score: 0,
-                problems: []
+                problems: [],
+                problemImage: "",
+                breed: "",
+                link: ""
             },
             {
                 name: "Semi-Prick Ears",
                 imageID: "ear2",
-                risk: "Medium",
+                buttonImage: "../img/dog/ear/ear2.png",
+                risk: "Low",
                 //score: 1,
-                problems: [
-                    "Ear infection"
-                ]
+                problems: [],
+                problemImage: "",
+                breed: "",
+                link: ""
             },
             {
                 name: "Folded Ears",
                 imageID: "ear3",
-                risk: "High",
+                buttonImage: "../img/dog/ear/ear3.png",
+                risk: "Medium",
                 //score: 2,
                 problems: [
-                    "Poor airflow",
-                    "Ear infection"
-                ]
+                    "Reduced ventilation",
+                    "Ear inflammation"
+                ],
+                problemImage: "../img/dog/problems/ear/ear3-problem.png",
+                breed: "Vizsla / Beagle",
+                link: "https://pmc.ncbi.nlm.nih.gov/articles/PMC8422687/"
             },
             {
                 name: "Drop Ears",
                 imageID: "ear4",
+                buttonImage: "../img/dog/ear/ear4.png",
                 risk: "High",
                 //score: 2,
                 problems: [
-                    "Ear infection",
-                    "Ear mites",
-                    "Wax build-up"
-                ]
+                    "Otitis externa",
+                    "yeast/bacterial infection"
+                ],
+                problemImage: "../img/dog/problems/ear/ear4-problem.png",
+                breed: "Cocker Spaniel / Basset hounds",
+                link: "https://www.acvs.org/small-animal/otitis-externa/"
             }
         ]
     },
@@ -150,39 +197,53 @@ const dogQuestion = [
             {
                 name: "Otter Tail",
                 imageID: "tail1",
+                buttonImage: "../img/dog/tail/tail1.png",
                 risk: "Low",
                 //score: 0,
-                problems: []
+                problems: [],
+                problemImage: "",
+                breed: "Labrador Retriever",
+                link: ""
             },
             {
                 name: "Curled Tail",
                 imageID: "tail2",
-                risk: "Medium",
+                buttonImage: "../img/dog/tail/tail2.png",
+                risk: "Low",
                 //score: 1,
-                problems: [
-                    "Tail stiffness"
-                ]
+                problems: [],
+                problemImage: "",
+                breed: "Shiba Inu",
+                link: ""
             },
             {
                 name: "Screw Tail",
                 imageID: "tail3",
+                buttonImage: "../img/dog/tail/tail3.png",
                 risk: "High",
                 //score: 2,
                 problems: [
-                    "Spinal problems",
-                    "Tail pain"
-                ]
+                    "Hemivertebrae",
+                    "Spinal cord/neurological problems",
+                    "hind-limb weakness"
+                ],
+                problemImage: "../img/dog/problems/tail/tail3-problem.png",
+                breed: "pug / Boston Terrier",
+                link: "https://www.battersea.org.uk/what-we-do/animal-welfare-campaigning/tackling-harmful-trends/screw-tails"
             },
             {
                 name: "Bobtail / Tailless",
                 imageID: "tail4",
+                buttonImage: "../img/dog/tail/tail4.png",
                 risk: "High",
                 //score: 2,
                 problems: [
-                    "Spinal problems",
-                    "Nerve issues",
-                    "Mobility issues"
-                ]
+                    "Anorectal atresia",
+                    " Multiple Spinal Defects"
+                ],
+                problemImage: "../img/dog/problems/tail/tail4-problem.png",
+                breed: "Pembroke Welsh Corgi",
+                link: "https://pubmed.ncbi.nlm.nih.gov/17850278/"
             }
         ]
     }
